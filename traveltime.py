5b70e5456715dfe0fbb01c19c6db0f3114a9addd
BenW0 <benweiss@uw.edu>
2016-04-28 21:57:54 -0700

    Added bidirectional travel; multiple origins

--- traveltime.py
+++ traveltime.py
@@ -16,27 +16,45 @@ with open("key.secret") as fin:
 
 gmaps = googlemaps.Client(key=key)
 
-result = gmaps.distance_matrix(params['Origin'], params['Destinations'].keys(), departure_time="now", units="imperial")
-print result
+res_forward = gmaps.distance_matrix(params['Origins'].keys(), params['Destinations'].keys(), departure_time="now", units="imperial")
+res_reverse = gmaps.distance_matrix(params['Destinations'].keys(), params['Origins'].keys(), departure_time="now", units="imperial")
+print res_forward
 
 # Output: Datetime, followed by distance, travel time, and travel time with traffic to each destination
 out = datetime.now().isoformat(' ') + ','
 
 for i in range(len(params['Destinations'].keys())):
-    out += '%f,' % (result['rows'][0]['elements'][i]['distance']['value'] * 0.000621371192)
-    out += '%f,' % (result['rows'][0]['elements'][i]['duration']['value'] / 60.0)
-    out += '%f,' % (result['rows'][0]['elements'][i]['duration_in_traffic']['value'] / 60.0)
+    for j in range(len(params['Origins'].keys())):
+        out += '%f,' % (res_forward['rows'][j]['elements'][i]['distance']['value'] * 0.000621371192)
+        out += '%f,' % (res_forward['rows'][j]['elements'][i]['duration']['value'] / 60.0)
+        out += '%f,' % (res_forward['rows'][j]['elements'][i]['duration_in_traffic']['value'] / 60.0)
+
+for i in range(len(params['Destinations'].keys())):
+    for j in range(len(params['Origins'].keys())):
+        out += '%f,' % (res_reverse['rows'][i]['elements'][j]['distance']['value'] * 0.000621371192)
+        out += '%f,' % (res_reverse['rows'][i]['elements'][j]['duration']['value'] / 60.0)
+        out += '%f,' % (res_reverse['rows'][i]['elements'][j]['duration_in_traffic']['value'] / 60.0)
 
 print out
 
 if not os.path.exists('log.csv'):
     with open('log.csv', 'w') as fout:
         fout.write('Time,')
-        for item in params['Destinations'].values():
-            fout.write(item + ' Dist (mi),')
-            fout.write(item + ' Time (min),')
-            fout.write(item + ' Time w/ Traffic (min),')
+        for dest in params['Destinations'].values():
+            for src in params['Origins'].values():
+                fout.write('%s to %s Dist (mi),' % (src, dest))
+                fout.write('%s to %s Time (min),' % (src, dest))
+                fout.write('%s to %s Time w/ Traffic (min),' % (src, dest))
+
+        for dest in params['Destinations'].values():
+            for src in params['Origins'].values():
+                fout.write('%s to %s Dist (mi),' % (dest, src))
+                fout.write('%s to %s Time (min),' % (dest, src))
+                fout.write('%s to %s Time w/ Traffic (min),' % (dest, src))
         fout.write('\n')
 
 with open('log.csv', 'a') as fout:
     fout.write(out + '\n')
+
+with open('dump.txt', 'a') as fout:
+    fout.write(str(res_forward) + '::::' + str(res_reverse) + '\n')
